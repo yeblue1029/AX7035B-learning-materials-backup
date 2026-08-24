f9b9108fe3dba1369c1e9101fd5eccb53640fd7c
yeblue1029 <yeblueyxb@outlook.com>
2026-08-24 16:45:55 +0800

    demo: add 11_3_an070_lcd_test (142 files, 3.4 MiB)

---
+++ 01_demo_document/demo/11_3_an070_lcd_test/lcd_test.ip_user_files/sim_scripts/video_pll/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "D:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "D:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../lcd_test.srcs/sources_1/ip/video_pll/video_pll_clk_wiz.v" \
+  "../../../../lcd_test.srcs/sources_1/ip/video_pll/video_pll.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ 01_demo_document/demo/11_3_an070_lcd_test/lcd_test.srcs/sources_1/src/top.v
@@ -0,0 +1,90 @@
+//////////////////////////////////////////////////////////////////////////////////
+//   lcd color bar test                                                         //
+//                                                                              //
+//  Author: meisq                                                               //
+//          msq@qq.com                                                          //
+//          ALINX(shanghai) Technology Co.,Ltd                                  //
+//          heijin                                                              //
+//     WEB: http://www.alinx.cn/                                                //
+//     BBS: http://www.heijin.org/                                              //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
+//                                                                              //
+// Copyright (c) 2017,ALINX(shanghai) Technology Co.,Ltd                        //
+//                    All rights reserved                                       //
+//                                                                              //
+// This source file may be used and distributed without restriction provided    //
+// that this copyright statement is not removed from the file and that any      //
+// derivative work contains the original copyright notice and the associated    //
+// disclaimer.                                                                  //
+//                                                                              //
+//////////////////////////////////////////////////////////////////////////////////
+
+//================================================================================
+//  Revision History:
+//  Date          By            Revision    Change Description
+//--------------------------------------------------------------------------------
+//2019/7/20                    1.0          Original
+//*******************************************************************************/
+module top(
+input                        sys_clk,          //system clock 50Mhz on board
+input                        rst_n,            //reset ,low active
+output                       lcd_pwm,          //LCD PWM backlight control	
+output                       lcd_dclk,         //lcd clock
+output                       lcd_hs,           //lcd horizontal synchronization
+output                       lcd_vs,           //lcd vertical synchronization  
+output                       lcd_de,           //lcd data valid
+output[7:0]                  lcd_r,            //lcd red
+output[7:0]                  lcd_g,            //lcd green
+output[7:0]                  lcd_b             // lcd blue
+);
+wire                        video_clk;        //pixel clock
+wire                        video_hs;         //horizontal synchronization
+wire                        video_vs;         //vertical synchronization
+wire                        video_de;         //video valid
+wire[7:0]                   video_r;          //video red data
+wire[7:0]                   video_g;          //video green data
+wire[7:0]                   video_b;          //video blue data
+
+assign lcd_hs = video_hs;
+assign lcd_vs = video_vs;
+assign lcd_de = video_de;
+assign lcd_r  = video_r[7:0];
+assign lcd_g  = video_g[7:0];
+assign lcd_b  = video_b[7:0];
+assign lcd_dclk = ~video_clk;                   //to meet the timing requirements, the clock is inverting
+/*************************************************************************
+Generate the pixel clock  required for the video
+****************************************************************************/
+video_pll video_pll_m0
+(
+.clk_in1                    (sys_clk                  ),
+.clk_out1                   (video_clk                ),
+.reset                      (1'b0                     ),
+.locked                     (                         )
+);
+/*************************************************************************
+Generating a fixed periodic pulse
+****************************************************************************/
+ax_pwm#(.N(24)) //pass new parameters
+ax_pwm_m0(
+.clk                        (sys_clk                  ),
+.rst                        (~rst_n                   ),
+.period                     (24'd17                   ),
+.duty                       (24'd5033164              ),
+.pwm_out                    (lcd_pwm                  )
+);    
+/*************************************************************************
+Call color bar generation module
+****************************************************************************/
+color_bar color_bar_m0(
+.clk                        (video_clk                ),
+.rst                        (~rst_n                   ),
+.hs                         (video_hs                 ),
+.vs                         (video_vs                 ),
+.de                         (video_de                 ),
+.rgb_r                      (video_r                  ),
+.rgb_g                      (video_g                  ),
+.rgb_b                      (video_b                  )
+);
+endmodule

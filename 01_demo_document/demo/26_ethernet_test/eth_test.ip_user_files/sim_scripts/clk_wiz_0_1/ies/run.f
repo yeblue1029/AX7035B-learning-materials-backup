854545f9f3f48b0fad94fc72996dcca1d948bc69
yeblue1029 <yeblueyxb@outlook.com>
2026-08-24 17:28:41 +0800

    demo: add 26_ethernet_test (1553 files, 201.9 MiB)

---
+++ 01_demo_document/demo/26_ethernet_test/eth_test.ip_user_files/sim_scripts/clk_wiz_0_1/ies/run.f
@@ -0,0 +1,15 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "G:/xilinx_2019_1/Vivado/2019.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "G:/xilinx_2019_1/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "G:/xilinx_2019_1/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../eth_test.srcs/sources_1/ip/clk_wiz_0_1/clk_wiz_0_clk_wiz.v" \
+  "../../../../eth_test.srcs/sources_1/ip/clk_wiz_0_1/clk_wiz_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+

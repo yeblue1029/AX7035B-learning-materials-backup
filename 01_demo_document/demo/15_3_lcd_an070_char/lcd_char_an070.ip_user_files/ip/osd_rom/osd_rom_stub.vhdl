3c5ab77da071838baa4cb04baad96dca67425f99
yeblue1029 <yeblueyxb@outlook.com>
2026-08-24 17:16:37 +0800

    demo: add 15_3_lcd_an070_char (232 files, 18.5 MiB)

---
+++ 01_demo_document/demo/15_3_lcd_an070_char/lcd_char_an070.ip_user_files/ip/osd_rom/osd_rom_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+-- Date        : Tue Aug  6 10:18:39 2019
+-- Host        : PC-201805041311 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               I:/ax7035_2019.1/demo/15_3_lcd_an070_char/lcd_an070_char/lcd_char_an070.srcs/sources_1/ip/osd_rom/osd_rom_stub.vhdl
+-- Design      : osd_rom
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tfgg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity osd_rom is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 10 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end osd_rom;
+
+architecture stub of osd_rom is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[10:0],douta[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_3,Vivado 2019.1";
+begin
+end;

eedf10a1d9db7996fc865a4ea35ff4b3447b8fc0
yeblue1029 <yeblueyxb@outlook.com>
2026-08-24 17:13:35 +0800

    demo: add 13_audio_record_play (454 files, 47.2 MiB)

---
+++ 01_demo_document/demo/13_audio_record_play/audio_record_play.ip_user_files/sim_scripts/clk_200M/xcelium/run.f
@@ -0,0 +1,15 @@
+-makelib xcelium_lib/xil_defaultlib -sv \
+  "D:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "D:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "D:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../audio_record_play.srcs/sources_1/ip/clk_200M/clk_200M_clk_wiz.v" \
+  "../../../../audio_record_play.srcs/sources_1/ip/clk_200M/clk_200M.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
